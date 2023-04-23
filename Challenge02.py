8fffa7510da410f3d1f445e555d99abaae7f3b9b
diohen90 <diohen90@gmail.com>
2023-04-23 19:57:07 +0100

    Testing

--- Challenge02.py
+++ Challenge02.py
@@ -1,1 +1,10 @@
+# Script : Challenge02.py
+# Purpose: Python script that sends ICMP requests to a host to evaluate if they are up or down.
 #!/usr/bin/python3
+
+import time
+import pythonping
+
+while True:
+    current_time = time.ctime()
+    print("Current time is: %s" % current_time)
