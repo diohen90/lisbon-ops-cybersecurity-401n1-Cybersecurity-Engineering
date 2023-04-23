269ffae1c5ef3b0a81c1e0039901d680da24159a
diohen90 <diohen90@gmail.com>
2023-04-23 20:48:32 +0100

    Testing

--- Challenge02.py
+++ Challenge02.py
@@ -3,8 +3,29 @@
 #!/usr/bin/python3
 
 import time
-import pythonping
+import os
+from datetime import datetime
+
+# IP address to ping
+ip = '8.8.8.8'
+# Time interval between pings
+interval = 2
 
 while True:
-    current_time = time.ctime()
-    print("Current time is: %s" % current_time)
+    # Current timestamp
+    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+
+    # Send the ICMP packet and get the return status
+    status = os.system(f'ping -c 1 {ip} > /dev/null 2>&1')
+
+    # Evaluate the return status and assign a success or failure status
+    if status == 0:
+        status_text = 'Success'
+    else:
+        status_text = 'Failure'
+
+    # Print the status and timestamp
+    print(f'{now} - {ip}: {status_text}')
+
+    # Wait for the specified interval before sending the next ICMP packet
+    time.sleep(interval)
